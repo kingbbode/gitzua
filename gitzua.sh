e2fcccbb1f110e9c41d93c2a021da47c1bd674e0
kingbbode <kingbbode@gmail.com>
2018-03-14 22:36:58 +0900

    input value validation

--- gitzua.sh
+++ gitzua.sh
@@ -19,17 +19,50 @@ find_git_host() {
 }
 
 create_info() {
-  printf "email : "
-  read email
-  #TODO: validation
-  printf "name : "
-  read name
-  #TODO: validation
-  echo "$email $name"
+  while : ; do
+    printf "email > " 
+    read email
+
+    if [[ -z "$email" ]]; then
+      printf '%s\n' "email is required!"
+      continue
+    else
+      break;
+    fi
+  done
+  
+  while : ; do
+    printf "name > "
+    read name
+
+    if [[ -z "$name" ]]; then
+      printf '%s\n' "name is required!"
+      continue
+    else
+      break;
+    fi
+  done
+
+  echo "\nCreate Public Key...\n"
   ssh-keygen -t rsa -C "$email" -f ~/.ssh/id_rsa_gitzua_$name
   write_ssh_config $email $name
 }
 
+required_input(){
+  while : ; do
+    printf "aaaaaaaaaaaa : "
+    read value
+
+    if [[ -z "$value" ]]; then
+      printf '%s\n' "$0 is required!"
+      continue
+    else
+      break;
+    fi
+  done
+  
+  echo "$value"
+}
 write_ssh_config() {
   email="$1"
   name="$2"
